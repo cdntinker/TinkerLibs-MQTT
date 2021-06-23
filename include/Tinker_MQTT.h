1e52ed2ed36a9e8346146ec5ae623d79a4f70629
cdntinker <cdntinker@gmail.com>
2021-06-23 15:06:00 -0400

    9

--- include/Tinker_MQTT.h
+++ include/Tinker_MQTT.h
@@ -1,29 +1,31 @@
-// #ifndef MQTT_H
-// #define MQTT_H
+#ifndef MQTT_H
+#define MQTT_H
 
-// #define MQTT_BUFFER_SIZE (100)       // This number is arbitrary
-//                                     // Topic can be up to 65,536 bytes
-//                                     // Message can be up to 268,435,456 bytes
+#define MQTT_BUFFER_SIZE (100)       // This number is arbitrary
+                                    // Topic can be up to 65,536 bytes
+                                    // Message can be up to 268,435,456 bytes
 
-// char MQTT_outTopic[MQTT_BUFFER_SIZE];
-// char MQTT_inTopic[MQTT_BUFFER_SIZE];
-// char MQTT_teleTopic[MQTT_BUFFER_SIZE];
-// char MQTT_statTopic[MQTT_BUFFER_SIZE];
+char MQTT_outTopic[MQTT_BUFFER_SIZE];
+char MQTT_inTopic[MQTT_BUFFER_SIZE];
+char MQTT_teleTopic[MQTT_BUFFER_SIZE];
+char MQTT_statTopic[MQTT_BUFFER_SIZE];
 
-// char MQTT_msg_out[MQTT_BUFFER_SIZE *10];
+char MQTT_msg_out[MQTT_BUFFER_SIZE *10];
 
-// unsigned long beacon_timer = 0;
-// #define BEACON_INTERVAL 30000 // Timer interval for the "keep-alive" status beacon
+unsigned long beacon_timer = 0;
+#define BEACON_INTERVAL 30000 // Timer interval for the "keep-alive" status beacon
 
-// extern const char* MQTTbroker;
-// extern const char* MQTTuser;
-// extern const char* MQTTpassword;
+extern const char* MQTTbroker;
+extern const char* MQTTuser;
+extern const char* MQTTpassword;
 
-// const char MQTT_ClientName[] = STR(DeviceName);
+const char MQTT_ClientName[] = STR(DeviceName);
 
-// extern const char* ssid;
+extern const char* ssid;
 
-// #endif  // MQTT_H
+#endif  // MQTT_H
+
+// #include "Tinker_MQTT.h"
 
 // MQTT ////////////////////////////////////////////////////////////////////////
 void MQTT_init();
